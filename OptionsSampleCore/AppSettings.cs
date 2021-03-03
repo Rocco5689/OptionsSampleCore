d6d0bd69aec3cf4192bf9594ba4a2555f578396b
Matthew Cavallo <macavall@microsoft.com>
2021-03-03 11:09:46 -0600

    Add project files.

---
+++ OptionsSampleCore/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OptionsSampleCore
+{
+    public class AppSettings
+    {
+        public string TempDirectory { get; set; }
+    }
+}
